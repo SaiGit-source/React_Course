37b6baf00fac37f6d6849f449a01a0a68b2b853f
Sai Gopinath <sainath.gopinath@gmail.com>
2025-01-23 17:39:54 -0500

    React: TimerChallenge app added

--- RefsPortals/TimerChallenge/Portals/src/components/ResultModal.jsx
+++ RefsPortals/TimerChallenge/Portals/src/components/ResultModal.jsx
@@ -1,18 +1,41 @@
 // a pop-up to see our result and score
 // the closer we are to timer expiring higher the score should be
 // we wrap the ResultModal function because it is going to receive the 'ref' prop
-import {forwardRef} from 'react'
+import {forwardRef, useImperativeHandle, useRef} from 'react'
+import { createPortal } from 'react-dom' // deeply nested elements could be hidden by other elements above it. 
+// teleport an HTML code into a different place in the DOM
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
-    return (
-    <dialog ref={ref} className="result-modal">
-        <h2>You {result}</h2>
+const ResultModal = forwardRef(function ResultModal({remainingTime, targetTime, onReset}, ref) {
+
+    // if different developers work on different components so they can change the function as they like
+    // useImperativeHandle() -> we can call this hook to define properties and methods that should be accessible on this component from outside this component
+    const dialogRef = useRef()
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                // this method is callable from outside function
+                dialogRef.current.showModal()
+            }
+        }
+    }) // now this component is reusable
+    // detaching dialogRef here and in the Timer challenge make the app flexible. for example, i could even change <dialog> to <div> etc
+
+    const userLost = remainingTime<=0
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2) // only 2 decimal places
+    const score = Math.round((1 - (remainingTime / (targetTime*1000)))*100)
+
+    return createPortal(
+        <dialog ref={dialogRef} className="result-modal">
+        {userLost && <h2>You lost!</h2>}
+        {!userLost && <h2>Your score: {score}</h2>}
         <p>The Target time was <strong>{targetTime} seconds. </strong></p>
-        <p>You stopped the timer with X seconds left.</p>
-        <form method="dialog">
+        <p>You stopped the timer with {formattedRemainingTime} seconds left.</p>
+        <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal') // we get modal id from index.html, where this HTML should be teleported to
     )
 }); // we wrap the ResultModal function because it is going to receive the ref prop
 
--- RefsPortals/TimerChallenge/Portals/src/components/TimerChallenge.jsx
+++ RefsPortals/TimerChallenge/Portals/src/components/TimerChallenge.jsx
@@ -7,26 +7,54 @@ export default function TimerChallenge({title, targetTime}) {
   const [timerStarted, setTimerStarted] = useState(false)
 
   const timer = useRef() // every component instance will get its own timer ref, which will never be overwritten, cleared or reset unlike variable
-  const dialogRef = useRef() // we are using this Ref to open the dialog programatically
+  const dialogRefTimer = useRef() // we are using this Ref to open the dialog programatically
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000)
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000
+  if (timeRemaining <=0){
+    //setInterval doesn't stop on its own
+    clearInterval(timer.current)
+    dialogRefTimer.current.open()
+  } // here we didnt stop timer in time so it expired so we lost
+
+  function handleTimeReset() {
+    setTimeRemaining(targetTime * 1000)
+  }
 
   function handleStart() {
     setTimerStarted(true)
-    timer.current = setTimeout(
+    /* timer.current = setTimeout(
       ()=>{setTimerExpired(true);
-        dialogRef.current.showModal(); // we are opening the modal only when times-out
+        dialogRefTimer.current.open(); // we are opening the modal only when times-out
+        // this open() method is referred from ResultModal
       }, 
       targetTime * 1000) // milliseconds
+      */
+
+          // we are using setInterval instead of setTimeout
+          // i want to execute every 10 milliseconds and we keep track of remaining time in our state
+      timer.current = setInterval(
+        ()=>{
+          setTimeRemaining(prevTimeRem => prevTimeRem - 10) // every 10ms we deduct 10ms from initial targetTime
+        }, 10) // milliseconds
+  
   }
 
   function handleStop() {
-    clearTimeout(timer.current) // we need to provide id of timer to stop it
+    // clearTimeout(timer.current) // we need to provide id of timer to stop it
     // also we dont want to update UI when we stop timer
+    dialogRefTimer.current.open()
+    clearInterval(timer.current)
   }
 
     return (
       <>
-      <ResultModal ref={dialogRef} targetTime={targetTime} result="lost">
+      <ResultModal 
+        ref={dialogRefTimer} 
+        targetTime={targetTime} 
+        remainingTime={timeRemaining}
+        onReset={handleTimeReset}>
       </ResultModal>
+
         <section className="challenge">
             <h2>{title}</h2>
             {/*{timerExpired && <p>You Lost!</p>}*/}
@@ -34,12 +62,13 @@ export default function TimerChallenge({title, targetTime}) {
                 {targetTime} second{targetTime>1 ? 's' : ''}
             </p>
             <p>
-            <button onClick={timerStarted ? handleStop : handleStart}>
+            <button onClick={timerIsActive ? handleStop : handleStart}>
                 {timerStarted ? 'Stop' : 'Start'} Challenge
             </button>
             </p>
-            <p className={timerStarted ? 'active' : undefined}>
-                {timerStarted ? 'Time is running...' : 'Timer inactive'}
+            {/* timerIsActive instead of timerStarted */}
+            <p className={timerIsActive ? 'active' : undefined}>
+                {timerIsActive ? 'Time is running...' : 'Timer inactive'}
             </p>
         </section>
         </>

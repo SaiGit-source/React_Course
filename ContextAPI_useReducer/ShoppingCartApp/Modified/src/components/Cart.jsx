2f2b1d738afe468051b86c45f9c8585a96a011bc
Sai Gopinath <sainath.gopinath@gmail.com>
2025-02-11 23:57:11 -0500

    React: ContextAPI part1 material added

---
+++ ContextAPI_useReducer/ShoppingCartApp/Modified/src/components/Cart.jsx
@@ -0,0 +1,50 @@
+import { useContext, useReducer } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+// useContext hook is to consume or access some context
+// use hook is bit more flexble than useContext. for example it can be used within an if block. only available in React v >= 19
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const cartCtx = useContext(CartContext);
+  // or const {items} = useContext(CartContext); // and change cartCtx.items to items
+
+  const totalPrice = cartCtx.items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+  return (
+    <div id="cart">
+      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+      {cartCtx.items.length > 0 && (
+        <ul id="cart-items">
+          {cartCtx.items.map((item) => {
+            const formattedPrice = `$${item.price.toFixed(2)}`;
+
+            return (
+              <li key={item.id}>
+                <div>
+                  <span>{item.name}</span>
+                  <span> ({formattedPrice})</span>
+                </div>
+                <div className="cart-item-actions">
+                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                    -
+                  </button>
+                  <span>{item.quantity}</span>
+                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                    +
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <p id="cart-total-price">
+        Cart Total: <strong>{formattedTotalPrice}</strong>
+      </p>
+    </div>
+  );
+}

a43394a088ca0c0084a5d82e2ab146510985846e
Sai Gopinath <sainath.gopinath@gmail.com>
2025-02-24 23:46:17 -0500

    React: Placepicker App finished

--- ReactSideEffects/PlacepickerApp/Modified/src/App.jsx
+++ ReactSideEffects/PlacepickerApp/Modified/src/App.jsx
@@ -7,10 +7,18 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+// i want to load userSelectedPlaces into storedIds when the App starts, only once
+const storedIds = JSON.parse(localStorage.getItem('userSelectedPlaces')) || []
+const storedPlaces = storedIds.map(id => 
+  AVAILABLE_PLACES.find((place)=>place.id===id)
+) //convert IDs into Places
+// it gives array of Place object based on array of IDs stored
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  //const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [placesSorted, setSortedPlaces] = useState([]);
 
   useEffect(() => {
@@ -26,6 +34,7 @@ function App() {
 
 // Actually React only executes the side effect / useEffect() only once after the App component is done
 // however, without empty '[]' dependency array, side-effect function will get executed after every App component render cycle causing an infinite loop
+
 
 
   function handleStartRemovePlace(id) {
@@ -45,14 +54,35 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
-  }
+
+    // to store selected places locally so we dont lose them when we re-load the page
+    // we will store data in browser's storage
+    // JSON.stringify takes in an array and converts into String
+    const storedIds = JSON.parse(localStorage.getItem('userSelectedPlaces')) || [] // parse is the opposite of stringfy() method
+    if (storedIds.indexOf(id)===-1){
+      localStorage.setItem(
+        'userSelectedPlaces', 
+        JSON.stringify([id, ...storedIds])
+      )
+  } // we cannot use useEffect() as well because useEffect() must be used directly at the root level of the component just like useState()
+  // also we need to store the places only when the user clicks the place not when the App component loads, unlike the Navigator code above. so this condition is different
+  // it will not cause an infinite loop even if we are updating a state value here
+}
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
-  }
+
+    // delete places from localStorage
+
+    const storedIds = JSON.parse(localStorage.getItem('userSelectedPlaces')) || [] // parse is the opposite of stringfy() method
+    localStorage.setItem(
+        'userSelectedPlaces', 
+        JSON.stringify(storedIds.filter((id) => id!==selectedPlace.current))
+      )
+  } // we cannot use useEffect() as well because useEffect() must be used directly at the root level of the component just like useState()
 
   return (
     <>
